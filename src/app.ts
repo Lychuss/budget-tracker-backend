8110c4d7a163906ec85c297d413cc5bd971eb21b
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-05-30 12:44:43 +0800

    fix the bug now

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,34 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/wallet-tune");
+
+//Global Error Handler
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    if(err instanceof Error){
+        return res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    };
+
+    return res.status(500).json({
+        message: "Unknown Error",
+        success: false
+    });
+});
+
+app.listen(5000, "0.0.0.0", () => {
+    console.log("Server port 5000 is listening...");
+})
